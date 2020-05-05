7f1d4f6db02947a435aee25453e51e848fdb190e
Kevin Luo <kevinluo@Kevins-MBP-2.home>
2020-05-05 14:46:37 -0400

    removing extra lines

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Dashboard from "./Dashboard.jsx";
-import HackerNews from "../containers/HackerNews"
-import Reddit from "../containers/Reddit.jsx"
 
 class App extends Component {
   constructor(props) {
