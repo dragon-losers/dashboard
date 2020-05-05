c43af353f89f41ff3a6558790f499ae64ccda586
Zac H <zac.haluza@gmail.com>
2020-05-05 15:04:39 -0400

    added layout state and methods for adding new components to dashboard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import Dashboard from "./Dashboard.jsx";
+import React, { useState } from 'react';
+import Dashboard from './Dashboard.jsx';
+import Sidebar from './Sidebar.js';
+import HackerNews from '../containers/HackerNews.js';
+import DevTo from '../containers/DevTo.js';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+const App = (props) => {
+  const [layout, setLayout] = useState([]);
 
-  render() {
-    return (
-      <div>
-        Dragon-Losers Forever!!
-        <Dashboard />
-      </div>
-    );
-  }
-}
+  const handleNewHackernews = () => {
+    setLayout([...layout, <HackerNews />]);
+  };
+
+  const handleNewDev = () => {
+    setLayout([...layout, <DevTo />]);
+  };
+
+  // const add
+
+  return (
+    <div className="app">
+      <Sidebar
+        handleNewHackernews={handleNewHackernews}
+        handleNewDev={handleNewDev}
+      />
+      <Dashboard hackernews={hackernews} dev={dev} />
+    </div>
+  );
+};
 
 export default App;
