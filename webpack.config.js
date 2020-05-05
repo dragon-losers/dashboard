87db6a0f4f10cd57d181d4d9f911c6d5abbb9e7a
Zac H <zac.haluza@gmail.com>
2020-05-05 10:51:52 -0400

    added sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ const path = require('path');
 module.exports = {
   entry: './src/index.jsx',
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   mode: process.env.NODE_ENV,
   devServer: {
-      compress: true,
-      port: 8080,
-      proxy: {
-          '/': {
-              target: 'http://localhost:3000',
-          },
+    compress: true,
+    port: 8080,
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
       },
-      publicPath: '/dist/',
+    },
+    publicPath: '/dist/',
   },
   module: {
-      rules: [
-          {
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use: {
-                  loader: 'babel-loader',
-                  options: {
-                      presets: ['@babel/preset-env', '@babel/preset-react'],
-                  },
-              },
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
-          {
-              test: /\.css$/,
-              exclude: /node_modules/,
-              use: ['style-loader', 'css-loader']
-          }
-      ]
-  }
-}
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
