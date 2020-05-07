78d9218e7616affdeab3d6b636a1223e3d3ff3be
Kevin Luo <kevinluo@Kevins-MBP-2.home>
2020-05-07 11:56:30 -0400

    bringing Reddit to work

--- src/containers/Reddit.jsx
+++ src/containers/Reddit.jsx
@@ -3,11 +3,7 @@ import SubReddit from '../components/SubReddit.jsx';
 
 const Reddit = (props) => {
     const [search, setSearch] = useState('');
-    const [subreddits, setSubreddits] = useState([]);
-    const subredditComps = subreddits.map(
-        (subreddit) => <SubReddit name={subreddit} />
-    )
-
+    const [subreddit, setSubreddit] = useState('');
 
     return (
         <div className={"redditContainer"}>
@@ -16,12 +12,8 @@ const Reddit = (props) => {
                 onChange={(e) => setSearch(e.target.value)}
                 value={search}
             />
-            <button onClick={() => {
-                console.log("Clicked!");
-                setSubreddits([...subreddits, search]);
-                console.log(subreddits);
-            }}>Add subreddit</button>
-            <div className={"subreddits"}>{subredditComps}</div>
+            <button onClick={() => setSubreddit(search)}>Add subreddit</button>
+            <SubReddit name={subreddit} />
         </div>
     )
 }
